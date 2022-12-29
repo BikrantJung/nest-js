dcd95c3eb66b24390c08178f032e6975cfb1e429
BikrantJung <bikrantjung1214@gmail.com>
2022-12-29 21:55:12 +0545

    Services and Controllers

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const PORT = 8000;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () =>
-    console.log(`App started at port http://localhost:${PORT}`),
+    console.log(`App started at port http://localhost:${PORT}`)
   );
 }
 bootstrap();
