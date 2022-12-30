5bf7a4fcaa94d6840c22ad5fe1e512d397f8da73
BikrantJung <bikrantjung1214@gmail.com>
2022-12-30 12:39:04 +0545

    Custom decorator for Request.user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
-import { Req } from "@nestjs/common/decorators";
-import { AuthGuard } from "@nestjs/passport";
-import { Request } from "express";
+import { User } from "@prisma/client";
+import { GetUser } from "src/auth/decorator";
 import { JWTGuard } from "src/auth/guard";
 
+@UseGuards(JWTGuard)
 @Controller("user")
 export class UserController {
-  @UseGuards(JWTGuard)
   @Get("me")
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User, @GetUser("email") email: string) {
+    // can also access email from user
+    console.log(email);
+    return user;
   }
 }
